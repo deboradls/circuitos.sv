6bab9a7d7997c448a14d966f64921a59f803eb25
Débora <deb.limad@gmail.com>
2023-12-24 20:14:49 -0300

    Registrador SISO - entrada serial e saída serial

---
+++ siso.sv
@@ -0,0 +1,15 @@
+module regSISO (
+    input logic clk, en, rst, din,
+    output logic dout
+);
+    logic [3:0] q_next, q_reg;
+    always_ff @(posedge clk, posedge rst)
+        begin
+            if (rst == 1'b1)
+                q_reg = 0;
+            else if (en == 1'b1)
+                q_reg = q_next;
+        end
+    assign q_next = {q_reg [2:0], din};
+    assign dout = q_reg [3];
+endmodule
