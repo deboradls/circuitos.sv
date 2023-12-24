0c4f2286abffddf0ae440f37b8a43f23eb75e3df
Débora <deb.limad@gmail.com>
2023-12-24 16:03:12 -0300

    FF tipo D

---
+++ ffd.sv
@@ -0,0 +1,13 @@
+module ffd (
+    input logic clock, reset, d,
+    output logic q
+);
+
+always_ff @(posedge clock, posedge reset) 
+    begin
+        if (reset == 1'b1)
+            q = 1'b0;
+        else
+            q = d;
+    end
+endmodule
